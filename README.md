d8416cd1e239a2edefe86990ab1d4aa38ff4327d
Oliver Haney <haney.oliver@gmail.com>
2020-05-30 22:06:32 -0400

    Add new frontend and update action workflows

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # secure-suite
+![frontend](https://github.com/haney-oliver/secure-suite/workflows/frontend/badge.svg)
+
+![flask backend](https://github.com/haney-oliver/secure-suite/workflows/flask%20backend/badge.svg)
+
 Secure Suite. An open source software package that uses machine learning to increase the user's general internet security.
---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="navbar">
+    <ul class="options">
+      <li>
+        <router-link to="/">Home</router-link>
+      </li>
+      <li @click="scrollTo('book-trailers')">
+        <a href="/#features">Feature</a>
+      </li>
+      <li @click="scrollTo('quickstart')">
+        <a href="/#quickstart">Quickstart</a>
+      </li>
+      <li v-show="user.loggedIn">
+        <router-link to="/dashboard">Dashboard</router-link>
+      </li>
+      <li v-show="user.loggedIn">
+        <router-link to="/vault">Vault</router-link>
+      </li>
+      <li v-show="user.loggedIn">
+        <router-link to="/preferences">Preferences</router-link>
+      </li>
+      <li v-if="user.loggedIn">
+        <a @click.prevent="signOut">Logout</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import firebaseApp from "@/main";
+import { mapGetters } from "vuex";
+import { EventBus } from "@/event-bus";
+import axios from "axios";
+import { BACKEND_URI } from "../main";
+import store from "../store";
+
+export default {
+  name: "Navbar",
+  data() {
+    return {
+      showList: false
+    };
+  },
+  methods: {
+    signOut() {
+      axios
+        .post(BACKEND_URI + "/api/LogoutUser", {
+          user_key: store.state.user_key
+        })
+        .then(store.dispatch("fetchUser", null))
+        .catch(response => console.log(response.message));
+    },
+    show() {
+      if (this.onBookTrailersPage) {
+        this.showList = true;
+      }
+    },
+    hide() {
+      this.showList = false;
+    },
+    scrollTo(ref) {
+      EventBus.$emit("scroll-to", ref);
+    }
+  },
+  computed: {
+    ...mapGetters({
+      user: "user"
+    })
+  }
+};
+</script>
+
+<style scoped>
+</style>f
